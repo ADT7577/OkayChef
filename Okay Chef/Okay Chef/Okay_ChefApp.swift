28cd1162fb61d4e36458683b31dc06a7d7f4324b
Alexsa Tolentino <adt7577@nyu.edu>
2023-11-25 13:20:58 -0500

    Codable

---
+++ Okay Chef/Okay Chef/Okay_ChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  Okay_ChefApp.swift
+//  Okay Chef
+//
+//  Created by Alexsa Tolentino on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Okay_ChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
