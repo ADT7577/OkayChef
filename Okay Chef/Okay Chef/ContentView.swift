ad9eb7fa83b459792e47a9707002401d85b2d28d
Alexsa Tolentino <adt7577@nyu.edu>
2023-11-26 12:41:40 -0500

    nil

--- Okay Chef/Okay Chef/ContentView.swift
+++ Okay Chef/Okay Chef/ContentView.swift
@@ -2,10 +2,11 @@
 ////  ContentView.swift
 ////  Okay Chef
 ////
-////  Created by Alexsa Tolentino on 11/25/23.
+////  Created by Alexsa Tolentino .
 ////
  import SwiftUI
 
+//MODELS
 
 struct Meal:  Codable, Identifiable {
        let idMeal: String
@@ -15,10 +16,25 @@ struct Meal:  Codable, Identifiable {
     var id: String { idMeal }
 }
 
+struct Category: Codable, Identifiable {
+    let idCategory: String
+    let strCategory: String
+    let strCategoryThumb: String
+    
+    var id: String { idCategory }
+}
+
+//RESPONSES
+
 struct MealsResponse: Codable {
     let meals: [Meal]
 }
 
+struct CategoriesResponse: Codable {
+    let categories: [Category]
+}
+
+//DESSERT
 class ViewModel: ObservableObject {
     @Published var meals: [Meal] = []
     
@@ -28,7 +44,7 @@ class ViewModel: ObservableObject {
         }
         URLSession.shared.dataTask(with: url) { data, _, error in
                    if let error = error {
-                       print("Error fetching data: \(error.localizedDescription)")
+                       print("Error")
                        return
                    }
                    
@@ -48,14 +64,91 @@ class ViewModel: ObservableObject {
            }
        }
 
+//CATEGORY
+class CategoryViewModel: ObservableObject {
+    @Published var categories: [Category] = []
+    
+    func fetchCategories() {
+        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/categories.php") else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print("Error")
+                return
+            }
+            
+            guard let data = data else {
+                print("No category data received")
+                return
+            }
+            
+            do {
+                let response = try JSONDecoder().decode(CategoriesResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self.categories = response.categories
+                }
+            } catch {
+                print("Error")
+            }
+        }.resume()
+    }
+}
+
+//CATEGORIES VIEW
+struct CategoryListView: View {
+    @StateObject var viewModel = CategoryViewModel()
+    var body: some View {
+           NavigationView {
+               List(viewModel.categories) { category in
+                   NavigationLink(destination: DessertListView(category: category)) {
+                       HStack {
+                           AsyncImage(url: URL(string: category.strCategoryThumb)) { image in
+                               image.resizable().aspectRatio(contentMode: .fill).frame(width: 50, height: 50).cornerRadius(8)
+                           } placeholder: {
+                               ProgressView()
+                           }
+        
+                           Text(category.strCategory)
+                       }
+                   }
+               }
+               .navigationTitle("Categories")
+           }
+           .onAppear {
+               viewModel.fetchCategories()
+           }
+       }
+   }
+
+
+
+
+
+
+//DESSERT VIEW
 struct DessertListView: View {
+    let category: Category
     @StateObject var viewModel = ViewModel()
     
     var body: some View {
         NavigationView {
             List(viewModel.meals) { meal in
                 NavigationLink(destination: DetailView(meal: meal)) {
-                    Text(meal.strMeal)
+                    HStack {
+                        //IMAGES
+                        AsyncImage(url: URL(string: meal.strMealThumb)) { image in
+                                image.resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 70, height: 50)
+                                .cornerRadius(8)
+                            } placeholder: {
+                            ProgressView()
+                        }
+                        //TEXT
+                        Text(meal.strMeal)
+                    }
                 }
             }
             .navigationTitle("Desserts")
@@ -66,6 +159,7 @@ struct DessertListView: View {
     }
 }
 
+//DETAIL VIEW
 
 struct DetailView: View {
     let meal: Meal
@@ -80,8 +174,23 @@ struct DetailView: View {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//PREVIEW
 struct ContentView_Preview: PreviewProvider{
     static var previews: some View {
-        DessertListView()
+        CategoryListView()
     }
 }
--- Okay Chef/Okay Chef/Okay_ChefApp.swift
+++ Okay Chef/Okay Chef/Okay_ChefApp.swift
@@ -8,7 +8,7 @@ import SwiftUI
 struct Okay_ChefApp: App {
     var body: some Scene {
         WindowGroup {
-        DessertListView()
+        CategoryListView()
         }
     }
 }
